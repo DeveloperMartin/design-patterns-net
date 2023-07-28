f1becd6a044f6a77e402f78c81f83cd46f8f058d
Martin Fernandez <43618471+DeveloperMartin@users.noreply.github.com>
2023-07-28 12:11:51 -0300

    add todo-mal

---
+++ todo-mal/todo-mal/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace todo_mal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tipoDeCafe = "americano";
+            string cafe;
+            double costo;
+
+            if (tipoDeCafe == "americano")
+            {
+                cafe = "Café Americano";
+                costo = 1.0;
+                Console.WriteLine($"{cafe} - Costo: ${costo}");
+            }
+            else if (tipoDeCafe == "latte")
+            {
+                cafe = "Café Latte";
+                costo = 1.5;
+                Console.WriteLine($"{cafe} - Costo: ${costo}");
+            }
+            else
+            {
+                Console.WriteLine("Tipo de café desconocido");
+            }
+
+            tipoDeCafe = "latte";
+
+            if (tipoDeCafe == "americano")
+            {
+                cafe = "Café Americano";
+                costo = 1.0;
+                Console.WriteLine($"{cafe} - Costo: ${costo}");
+            }
+            else if (tipoDeCafe == "latte")
+            {
+                cafe = "Café Latte";
+                costo = 1.5;
+                Console.WriteLine($"{cafe} - Costo: ${costo}");
+            }
+            else
+            {
+                Console.WriteLine("Tipo de café desconocido");
+            }
+        }
+    }
+}
